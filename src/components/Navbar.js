785ca0e8a8bfdba3cac9f038ec143b074ab86074
alexanderys <alexander.yararsellgren@student.kyh.se>
2021-02-23 15:53:44 +0100

    fixed dropdown menu on mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { faCode, faBars } from '@fortawesome/free-solid-svg-icons';
+
+let dropHidden = true;
 
 function Navbar() {
+
+    function dropdown() {
+        console.log('clicked');
+        const dropdown = document.querySelector('.dropdown');
+        if (dropHidden) {
+            dropdown.style.display = 'block';
+            dropHidden = false;
+        } else {
+            dropdown.style.display = 'none';
+            dropHidden = true;
+        }
+    }
+
     return (
-        <nav>
-            <FontAwesomeIcon icon={faCode} size="3x" className="nav-icon" />
-            <ul>
+        <>
+            <nav>
+                <FontAwesomeIcon icon={faCode} size="3x" className="nav-icon" />
+                {/*  */}
+                <div>
+                    <button className="dropbtn">
+                        <FontAwesomeIcon icon={faBars} size="2x" className="menu-icon" onClick={dropdown} />
+                    </button>
+                    {/*  */}
+                    <ul className="dropdown-content">
+                        <li>
+                            <NavLink activeClassName="active" exact to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName="active" to="/about">About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName="active" to="/contact">Contact</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <ul className="dropdown">
                 <li>
-                    <NavLink activeClassName="active" exact to="/">Home</NavLink>
+                    <Link onClick={dropdown} exact to="/">Home</Link>
                 </li>
                 <li>
-                    <NavLink activeClassName="active" to="/about">About</NavLink>
+                    <Link onClick={dropdown} to="/about">About</Link>
                 </li>
                 <li>
-                    <NavLink activeClassName="active" to="/contact">Contact</NavLink>
+                    <Link onClick={dropdown} to="/contact">Contact</Link>
                 </li>
             </ul>
-        </nav>
+        </>
     );
 }
 
